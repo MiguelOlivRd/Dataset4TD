4ff58dfcd3f2085d25b0a26416acfb64b63b2da4
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2026-07-23 11:06:08 -0300

    identified duplicates

--- Miguel/utils.py
+++ Miguel/utils.py
@@ -2,7 +2,8 @@ import os
 import re
 import glob
 import pandas as pd
-
+from pathlib import Path
+from tqdm import tqdm
 
 def parse_rq3_table(file_path):
     """Reads a LaTeX table from a file and parses it into a clean Pandas DataFrame."""
@@ -143,3 +144,29 @@ def parse_within_project_file(filepath):
                 continue
 
     return pd.DataFrame(data)
+
+
+def read_data(GRANULARITIES, DF_FOLDER_PATH, PROJECTS):
+
+    def find_file(folder_path, prefix):
+        # folder.glob('prefix*') looks for anything starting with the prefix
+        folder = Path(folder_path)
+        return next(folder.glob(f"{prefix}*"))
+
+    # granularity -> project -> dataframe
+    data = {}
+
+    for granularity in tqdm(GRANULARITIES):
+        data[granularity] = {}
+        
+
+        for project in PROJECTS:
+            
+            all_projects_folder_path = f"{DF_FOLDER_PATH}/{granularity}/"
+            project_file = find_file(folder_path=all_projects_folder_path, prefix=project.lower())
+            
+            project_df = pd.read_csv(project_file)
+            project_name = project.split("-")[0]
+            data[granularity][project_name] = project_df
+
+    return data
